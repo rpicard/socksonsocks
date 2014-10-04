009b0f096cae201baa5655d26451ff0759cf8c00
Robert Picard <mail@robert.io>
2014-10-04 11:59:12 -0700

    Add setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name = 'socksonsocks',
+    version = '0.1',
+    description = 'A tool to set a socket up for using a SOCKS proxy',
+    author = 'Robert Picard',
+    author_email = 'robert@robert.io',
+    url = 'https://github.com/rpicard/socksonsocks',
+    packages = ['socksonsocks'],
+    license = 'MIT'
+)
