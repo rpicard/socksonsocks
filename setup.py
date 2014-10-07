a17885555f6896466a28727488c1203f66ae51a1
Robert Picard <mail@robert.io>
2014-10-07 16:46:46 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name = 'socksonsocks',
-    version = '0.1',
+    version = '0.2',
     description = 'A tool to set a socket up for using a SOCKS proxy',
     author = 'Robert Picard',
     author_email = 'robert@robert.io',
